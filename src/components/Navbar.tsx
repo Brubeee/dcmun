92f4e4d7ea435caafacff6f65d357bf73774602c
Brubeee <113605706+Brubeee@users.noreply.github.com>
2025-04-29 18:40:27 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -55,7 +55,7 @@ const Navbar = () => {
               {link.name}
             </a>
           ))}
-          <a href="https://docs.google.com/forms/d/19dwb4pjXc016A8wu2KFolLKIS1iX8PLwFYJMZDz62-c/edit" target="_blank" rel="noopener noreferrer">
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSczMZFNTdVQ41kAAZ6x_xa9xBiIz_2kHMFeO-zrtC7X8tkSxw/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">
             <Button className="bg-diplomacy-purple hover:bg-diplomacy-lightPurple text-white">
               Register Now
             </Button>
