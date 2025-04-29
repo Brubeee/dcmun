8c5287d4f80d6503f6ff31a6701887210bdca206
Brubeee <113605706+Brubeee@users.noreply.github.com>
2025-04-29 20:45:13 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -38,7 +38,7 @@ const Hero = () => {
             Experience diplomacy across timelines — from ancient civilizations to speculative futures. 
             A debate competition challenging your adaptability, strategy, and negotiation skills.
           </p>
-           <div className="absolute bottom-9 left-1/2 transform -translate-x-1/2 animate-bounce cursor-pointer" onClick={scrollToFeatures}>
+           <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce cursor-pointer" onClick={scrollToFeatures}>
             <ChevronDown className="text-diplomacy-lightPurple w-8 h-8" />
            </div>
           <br></br>
