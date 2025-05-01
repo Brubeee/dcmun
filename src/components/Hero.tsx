a4f460596a2ada31e516df45f47a6dcc7f531d9e
Brubeee <113605706+Brubeee@users.noreply.github.com>
2025-05-01 17:19:11 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -42,7 +42,7 @@ const Hero = () => {
           <img
               src="/logo (5).png"
               alt="DCMUN Logo"
-              className="mx-auto h-64 sm:h-80 w-auto mb-8 sm:mb-16"
+              className="mx-auto h-64 sm:h-80 w-auto mb-4 sm:mb-8"
           />
 
           {/* Large welcome text with better mobile scaling */}
